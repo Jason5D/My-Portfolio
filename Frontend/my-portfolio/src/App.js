6a40457cd726bc782eaa688ff3f7d6e1a9031337
Jason <jason5donoghue@gmail.com>
2024-02-12 21:13:12 +0000

    created react app and first component initial text

---
+++ Frontend/my-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Introduction from "./Introduction.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+        <Introduction />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/my-portfolio/src/Introduction.js
@@ -0,0 +1,17 @@
+export default function Introduction() {
+  return (
+    <div>
+    <p>Hi, my name is Jason Donoghue.</p>
+
+      <p>I have learnt how to create websites and digital experiences</p>
+
+      <p>I'm a developer specialising in Javascript and Python.</p>
+
+      <p>
+        Currently I'm making a career switch from technical sales and IT
+        engineering, into software development, and have a keen interest in
+        machine learning and data.
+      </p>
+      </div>
+  )
+}
