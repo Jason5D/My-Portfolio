1988a66bc3a87d8046f68393e3fd6bf8ce9c9fc4
Jason <jason5donoghue@gmail.com>
2024-02-14 21:26:45 +0000

    further styled navbar and added accesssibility

--- Frontend/my-portfolio/src/components/Navbar.js
+++ Frontend/my-portfolio/src/components/Navbar.js
@@ -4,7 +4,10 @@ export default function Navbar() {
       <ul>
         <li>
           <h1>
-            <a href="index.html">Jason Donoghue</a>
+            <a href="index.html">
+              <span class="fa-solid fa-frog" aria-hidden="true"></span>
+              <span>Jason Donoghue</span>
+            </a>
           </h1>
         </li>
         <li>
@@ -18,14 +21,18 @@ export default function Navbar() {
         </li>
         <li>
           <a href="https://linkedin.com/in/jason-donoghue-3765a6267">
-            LinkedIn
+            <span class="fa-brands fa-linkedin" aria-hidden="true"></span>
+            <span class="sr-only">LinkenIn</span>
           </a>
         </li>
         <li>
-          <a href="https://github.com/Jason5D">Github</a>
+          <a href="https://github.com/Jason5D">
+          <span class="fa-brands fa-github" aria-hidden="true"></span>
+          <span class="sr-only">Github</span>
+          </a>
         </li>
         <li>
-          <a href="/Users/jason5d/Library/Mobile Documents/com~apple~CloudDocs/CVs/Jason Donoghue CV C.docx">
+          <a href="/Users/jason5d/Library/Mobile Documents/com~apple~CloudDocs/CVs/Jason Donoghue CV C.docx" class="button">
             Resume
           </a>
         </li>
