753c4e65b8bfe3076b692505b582855a714506f1
Jason <jason5donoghue@gmail.com>
2024-02-15 22:08:34 +0000

    project section added with styling

--- Frontend/my-portfolio/src/components/Projects.js
+++ Frontend/my-portfolio/src/components/Projects.js
@@ -4,24 +4,26 @@ export default function Projects() {
       <section id="projects">
         <h2>Projects I'm proud of</h2>
         <article>
-          <h4>Latest Project</h4>
-          <h3>Studyosaurus</h3>
-          <p>
-            Description of the project. This should be faily concise while also
-            describing the key components that you developed or worked on. It
-            can be as long as you need it to be but should at least be a few
-            sentences long. Be sure to include specific links anywhere on the
-            description. A link looks like
-            <a href="https://studyosaurus.netlify.app/">this</a>.
-          </p>
-          <h4>Technologies used include:</h4>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>SVG</li>
-          </ul>
+          <div class="text">
+            <h4>Latest Project</h4>
+            <h3>Studyosaurus</h3>
+            <p class="blackbox">
+              Description of the project. This should be faily concise while
+              also describing the key components that you developed or worked
+              on. It can be as long as you need it to be but should at least be
+              a few sentences long. Be sure to include specific links anywhere
+              on the description. A link looks like
+              <a href="https://studyosaurus.netlify.app/">this</a>.
+            </p>
+            <h4>Technologies used include:</h4>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>SVG</li>
+            </ul>
+          </div>
           <img
-            src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="Man on a beach"
           />
         </article>
