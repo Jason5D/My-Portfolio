1adfa0db69c117eb443130768d26a4e447c06fad
Jason <jason5donoghue@gmail.com>
2024-02-24 13:25:39 +0000

    added option to view and download resume

--- Frontend/my-portfolio/src/components/Resume.js
+++ Frontend/my-portfolio/src/components/Resume.js
@@ -1,4 +1,12 @@
-export default function Resume() {
+import React from "react";
+import CV from "../content/CV.pdf";
+
+const Resume = () => {
+  const openPDF = () => {
+    const url = CV; // Adjust the path to your PDF file
+    window.open(url, "_blank");
+  };
+
   return (
     <div>
       <header>
@@ -6,92 +14,9 @@ export default function Resume() {
         <p>Email: jason5donoghue@gmail.com</p>
       </header>
 
-      <section>
-        <h2>PERSONAL STATEMENT</h2>
-        <p>
-          Proficient junior software engineer with experience of designing,
-          developing, and implementing software applications. Utilizing
-          JavaScript and Python to write. With a strong background in technical
-          sales roles, and supplying lighting technology to the construction
-          industry, I’ve witnessed the transformative power of technology
-          first-hand. My true passion lies in coding, which led me to enroll in
-          the School of Code’s intensive boot camp. There, I honed my
-          problem-solving skills and discovered how learning to code can drive
-          innovation. I have agreed to support pre-enrolment on the course, and
-          I also attend tech meetups to meet likeminded friends. In my spare
-          time I have been learning new languages, getting to grips with data
-          structures and algorithms, and have several personal projects I’m
-          working on.
-        </p>
-      </section>
-
-      <section>
-        <h2>SKILLS</h2>
-        <p>
-          HTML | CSS | Git | JavaScript | TypeScript | Java | Python React |
-          Next | Vue | Vite | Node | Express | RESTful API | Flask | SQL |
-          MongoDB
-        </p>
-      </section>
-
-      <section>
-        <h2>HOBBIES</h2>
-        <ul>
-          <li>Swim – How I enjoy keeping fit</li>
-          <li>Chess – Played for England in my youth.</li>
-          <li>Skiing – Reached a level to teach young children.</li>
-          <li>Trombone – Enjoyed playing in brass and wind bands.</li>
-          <li>Volleyball – Play mostly during the summer.</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>EDUCATION</h2>
-        <ul>
-          <li>2023 – School of Code</li>
-          <li>2022 – CompTIA A+ / 220 1101 + 1102</li>
-          <li>2019 – PRINCE 2 certification</li>
-          <li>2018 – Certified Associate in Project Management</li>
-          <li>2011 – BA Degree in Business Management & Sports</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>WORK EXPERIENCE</h2>
-        <article>
-          <h3>COMPUTER AND NETWORKS – July 2023</h3>
-          <p>HELPDESK TECHNICIAN</p>
-          <p>
-            Responsible for solving technical issue quickly and providing
-            hardware and software support. Currently designing a support tool to
-            combine troubleshooting information and customer data using
-            Javascript and Python. Using my strong IT knowledge, I work with the
-            team to solve computer and network issues quickly in a frontline IT
-            support role. I enjoy being in a technical environment and being
-            challenged with difficult scenarios, while helping people and
-            providing an essential service. I’ve continued to develop a high
-            attention to detail and logical problem-solving skills, while
-            searching for a developer role.
-          </p>
-        </article>
-      </section>
-      <section>
-        <h2>KEY PROJECTS 2023</h2>
-        <article>
-          <h3>STUDYOSAURUS</h3>
-          <p>
-            <strong>Problem:</strong> Bootcamp lacked resource management
-            system.
-            <strong>Collaborators:</strong> Victoria Platt, George Ussher, Evie
-            Amoura
-            <strong>Tech Stack:</strong> JavaScript, React, Vite
-            <strong>Approach:</strong> Dot voting, pair programming, roles,
-            pre-coding planning, refactoring.
-            <strong>Learnings:</strong> Equal opportunities, streamlined tech
-            stack, regular meetings, everyone understand projects.
-          </p>
-        </article>
-      </section>
+      <button onClick={openPDF}>Open CV</button>
     </div>
   );
-}
+};
+
+export default Resume;
